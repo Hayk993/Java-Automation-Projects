424ffe542121b7499406f40fdd5ae19beaeac3ad
Hayk993 <haykvm393@gmail.com>
2026-07-30 22:28:50 +0400

    All 27 cases

--- Lesson1/Lessons/src/homework/atm/Main.java
+++ Lesson1/Lessons/src/homework/atm/Main.java
@@ -5,23 +5,35 @@ public class Main {
 
         ATMService atm = new ATMImpl();
 
-        TestCase tc1 = new TestCase("DT-01", true, 500, 1000, 1000, "APPROVED");
+        runTest(atm, new TestCase("BVA-01", true, 99, 50000, 50000, "DENIED_INVALID_AMOUNT"));
+        runTest(atm, new TestCase("BVA-02", true, 100, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("BVA-03", true, 101, 50000, 50000, "DENIED_INVALID_AMOUNT"));
+        runTest(atm, new TestCase("BVA-04", true, 19999, 50000, 50000, "DENIED_INVALID_AMOUNT"));
+        runTest(atm, new TestCase("BVA-05", true, 20000, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("BVA-06", true, 20001, 50000, 50000, "DENIED_INVALID_AMOUNT"));
+        runTest(atm, new TestCase("BVA-07", true, 199, 50000, 50000, "DENIED_INVALID_AMOUNT"));
+        runTest(atm, new TestCase("BVA-08", true, 200, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("BVA-09", true, 201, 50000, 50000, "DENIED_INVALID_AMOUNT"));
 
-        runTest(atm, tc1);
-        // DT-01
+        runTest(atm, new TestCase("ST-01", true, 500, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("ST-02", true, 1000, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("ST-03", false, 500, 50000, 50000, "DENIED_ACCOUNT_BLOCKED"));
+        runTest(atm, new TestCase("ST-04", false, 1000, 50000, 50000, "DENIED_ACCOUNT_BLOCKED"));
+        runTest(atm, new TestCase("ST-05", true, 500, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("ST-06", true, 1000, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("ST-07", true, 500, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("ST-08", true, 1000, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("ST-09", false, 500, 50000, 50000, "DENIED_ACCOUNT_BLOCKED"));
+        runTest(atm, new TestCase("ST-10", true, 500, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("ST-11", true, 1000, 50000, 50000, "APPROVED"));
+        runTest(atm, new TestCase("ST-12", false, 500, 50000, 50000, "DENIED_ACCOUNT_BLOCKED"));
+        runTest(atm, new TestCase("ST-13", true, 500, 50000, 50000, "APPROVED"));
+
         runTest(atm, new TestCase("DT-01", true, 500, 1000, 1000, "APPROVED"));
-
-        // DT-02
         runTest(atm, new TestCase("DT-02", true, 500, 1000, 400, "DENIED_LIMIT_EXCEEDED"));
-//---------------------------------------------------------------------------------
-        //Amount = 99
-        runTest(atm, new TestCase("BVA-01", true, 99, 1000, 1000, "DENIED_INVALID_AMOUNT"));
-
-// Amount = 100
-        runTest(atm, new TestCase("BVA-02", true, 100, 1000, 1000, "APPROVED"));
-
-// Amount = 101
-        runTest(atm, new TestCase("BVA-03", true, 101, 1000, 1000, "DENIED_INVALID_AMOUNT"));
+        runTest(atm, new TestCase("DT-03", true, 500, 300, 1000, "DENIED_INSUFFICIENT_BALANCE"));
+        runTest(atm, new TestCase("DT-04", true, 250, 1000, 1000, "DENIED_INVALID_AMOUNT"));
+        runTest(atm, new TestCase("DT-05", false, 500, 1000, 1000, "DENIED_ACCOUNT_BLOCKED"));
     }
 
     public static void runTest(ATMService atm, TestCase testCase) {
